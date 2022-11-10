dd0209714a84df8ef2f62313503dc3ca7efdba2d
LuisCMalo <camino.malo@bootcamp-upgrade.com>
2022-11-10 07:58:28 +0100

    added delete contact option

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,5 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
 Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
 Route::put('/contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update');
+Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
 Route::post('/contacts/', [ContactController::class, 'store'])->name('contacts.store');
